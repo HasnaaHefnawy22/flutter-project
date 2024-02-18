27bd843ae2a6768ac35a2ca68e5ed5a91d63d91c
Hasnaa Elhefnawy <hasnaaahmed132@gmail.com>
2024-02-18 21:20:39 +0200

    hasnaa

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
-
+//ffffffffffffffffffffffffffffffffffffffffffffffffff
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'hasnaa1234',
+      title: 'hhhhhhhhhhhhhhhhhhhhh',
       theme: ThemeData(
         // This is the theme of your application.
         //
